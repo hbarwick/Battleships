819550e73ce22e2149762f1327ccc4ab4a7b516b
hbarwick <hallambarwick@hotmail.com>
2022-01-04 22:35:38 +0000

    Added Ship class, list of ships, and ship images

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+from pathlib import Path
 import sys
-import itertools
 import pygame
 from pygame.locals import *
-
-pygame.init()
-
-window_surface = pygame.display.set_mode((480, 480), 0, 32)
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -13,28 +9,23 @@ BLUE = (52, 140, 235)
 RED = (230, 15, 11)
 GREY = (107, 99, 99)
 
-title_font = pygame.font.SysFont(None, 42)
-text = title_font.render("Battleships!", True, BLACK, GREY)
-
-text_rect = text.get_rect()
-text_rect.centerx = window_surface.get_rect().centerx
-text_rect.centery = 20
-
-window_surface.fill(GREY)
-
-pygame.draw.polygon(window_surface, BLUE, ((40, 40), (440, 40), (440, 440), (40, 440)))
+SHIPS = {"Battleship": 5,
+         "Cruiser": 4,
+         "Submarine": 3,
+         "Rescue Ship": 3,
+         "Destroyer": 2,
+         "Aeroplane": 1}
 
 
-window_surface.blit(text, text_rect)
-
-pygame.display.update()
+pygame.init()
+window_surface = pygame.display.set_mode((480, 480), 0, 32)
 
 
 class Grid:
     def __init__(self, num_rows=10,
                  num_cols=10,
                  cell_width=40,
-                 grid_size=402, #  2 extra pixels to allow line width not to be cut off
+                 grid_size=402,  # 2 extra pixels to allow line width not to be cut off
                  x_loc=40,
                  y_loc=40):
         self.y_loc = y_loc
@@ -70,7 +61,6 @@ class Grid:
                 cell_x += self.cell_width
             cell_y += self.cell_width
 
-
     def get_cell(self):
         x, y = pygame.mouse.get_pos()
         gridx = x - self.x_loc
@@ -83,15 +73,21 @@ class Grid:
                     print(cell.row, cell.column)
                     cell.change_colour()
 
+    def add_ships(self):
+        pass
+
+
 class Cell:
-    def __init__(self, x_coord, y_coord, cell_width, row, column):
+    def __init__(self, x_coord: int, y_coord: int, cell_width: int, row: int, column: int):
         self.y_coord = y_coord
         self.x_coord = x_coord
         self.cell_width = cell_width
         self.row = row
         self.column = column
-        self.rect = pygame.Rect(self.x_coord + 43, self.y_coord + 43, self.cell_width - 3, self.cell_width - 3)
-        self.surface = pygame.Surface((self.cell_width - 4, self.cell_width - 4))
+        self.rect = pygame.Rect(self.x_coord + 43, self.y_coord + 43,
+                                self.cell_width - 3, self.cell_width - 3)  # -3 to stop cell fill overlap with border
+        self.surface = pygame.Surface((self.cell_width - 4,
+                                       self.cell_width - 4))  # -4 to stop cell surface overlapping with cell borders
 
     def change_colour(self):
         self.surface.fill(RED)
@@ -99,7 +95,25 @@ class Cell:
         pygame.display.flip()
 
 
+class Ship(pygame.sprite.Sprite):
+    def __init__(self, name: str, length: int, coordinates: tuple, image: Path, *groups: AbstractGroup):
+        super().__init__(*groups)
+        self.coordinates = coordinates
+        self.length = length
+        self.name = name
+        self.image = image
+
+
 def main():
+    title_font = pygame.font.SysFont(None, 42)
+    text = title_font.render("Battleships!", True, BLACK, GREY)
+    text_rect = text.get_rect()
+    text_rect.centerx = window_surface.get_rect().centerx
+    text_rect.centery = 20
+    window_surface.fill(GREY)
+    pygame.draw.polygon(window_surface, BLUE, ((40, 40), (440, 40), (440, 440), (40, 440)))
+    window_surface.blit(text, text_rect)
+    pygame.display.update()
     grid = Grid()
     grid.draw_grid()
     grid.create_cells()
@@ -107,6 +121,7 @@ def main():
         print(f"column {cell.column} - row{cell.row}")
     window_surface.blit(grid.surface, grid.rect)
     pygame.display.flip()
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -116,9 +131,5 @@ def main():
                 grid.get_cell()
 
 
-
-
-
 if __name__ == '__main__':
     main()
-
